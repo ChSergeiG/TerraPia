076c32263171ff691700fcaa0b55b311acc18627
ChSergeiG <chsergeyg@gmail.com>
2017-10-05 03:19:32 +0300

    updated climate logic

--- Arduino/terr.ino
+++ Arduino/terr.ino
@@ -15,13 +15,15 @@
 
 #define SYSTEM 13
 
-DHT11 dht(A0);
+#define UPDATE_DELAY 60
 
-boolean day;
-boolean smM;
-boolean bgM;
-boolean hum;
-int counter;
+DHT11 dht(HIM);
+
+boolean day = false;
+boolean bgM = false;
+boolean hum = false;
+boolean smM = false;
+int counter = UPDATE_DELAY;
 
 void setup() {
   dht.begin();
@@ -30,77 +32,62 @@ void setup() {
   pinMode(VCC_3, OUTPUT);
   pinMode(LUX, INPUT);
   pinMode(SYSTEM,OUTPUT);
+
+  digitalWrite(VCC_1,bgM?LOW:HIGH);
+  digitalWrite(VCC_2,hum?LOW:HIGH);
+  digitalWrite(VCC_3,smM?LOW:HIGH);
   digitalWrite(SYSTEM,LOW);
-  //digitalWrite(VCC_1, LOW);
-  //digitalWrite(VCC_2, HIGH);
-  //digitalWrite(VCC_3, LOW);
-  day = true;
-  smM = false;
-  bgM = true;
-  hum = false;
-  counter = 1000;
+  
   Serial.begin(9600);
 }
 
 void loop() {
   // get sensor data
+  String data;
   int check;
   int humidity;
   int temperature;
   int lux = analogRead(LUX);
   check = dht.read();
-  Serial.print(counter);
-  Serial.print(" ");
+  data = data + "[" + counter+"] ";
   switch (check) {
     case DHT_OK:
       temperature = dht.getTemperatureC();
       humidity = dht.getHumidity();
-      Serial.print(temperature);
-      Serial.print(" ");
-      Serial.print(humidity);
-      Serial.print(" ");
+      data = data + temperature + " "+ humidity+ " ";
       break;
     default:
-      Serial.print("Error Error");
+      data = data + "err err ";
       break;
   }
-  Serial.println(analogRead(LUX));
-
-  if (counter > 600) {
-  day = lux > 600;
-    // set season
-    // small heater
-    if (temperature <= 25 && !smM) {
-      smM = true;
-      digitalWrite(VCC_3, smM?LOW:HIGH);
-    } else {
-      smM = false;
-      digitalWrite(VCC_3, smM?LOW:HIGH);
-    }
+  data = data + lux + " ";
+  if (counter > UPDATE_DELAY) {
+    day = lux < 600;
+    /// set season
     // large heater
-    if (day && temperature <= 25) {
-      bgM = true;
-      digitalWrite(VCC_1, bgM?LOW:HIGH);
-    } else if (temperature <= 20) {
-      bgM = true;
-      digitalWrite(VCC_1, bgM?LOW:HIGH);
-    } else if (day && temperature > 20) {
-      bgM = false;
-      digitalWrite(VCC_1, bgM?LOW:HIGH);
+    if (day) {
+      bgM = temperature <= 25;
     } else {
-      bgM = false;
-      digitalWrite(VCC_1, bgM?LOW:HIGH);
+      bgM = temperature <= 20;
     }
+    digitalWrite(VCC_1, bgM?LOW:HIGH);
+    
     // humidifier
-    if (humidity < 20 && !hum) {
+    if (humidity < 20) {
       hum = true;
-      digitalWrite(VCC_2, hum?LOW:HIGH);
-    } else if (humidity > 40 && hum) {
+    } else if (humidity > 40) {
       hum = false;
-      digitalWrite(VCC_2, hum?LOW:HIGH);
     }
+    digitalWrite(VCC_2, hum?LOW:HIGH);
+
+    // small heater
+    smM = temperature <= 25;
+    digitalWrite(VCC_3, smM?LOW:HIGH);
+
     counter = 0;
-  }  
+  }
+  data = data + bgM + " " + hum + " " + smM;
+  Serial.println(data);
   counter++;
   delay(1000);
 }
